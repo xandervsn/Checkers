158f0389dbdedcd971c075be64917dd322f4db94
Xander Siruno-Nebel <73612166+xandervsn@users.noreply.github.com>
2022-05-25 12:22:15 -0700

    Update Checkers.java

--- src/group/six/Checkers.java
+++ src/group/six/Checkers.java
@@ -22,11 +22,36 @@ public class Checkers extends JFrame {
 	JButton forfeit = new JButton();
 	Clicklistener click = new Clicklistener();
 	
-	public Checkers() {
+	public Checkers() { //initializes the board
 		for (int y = 'a'; y < 'i'; y++) {
 			for (int x = 0; x < 8; x++) {
-				String tile = x + String.valueOf(y);
+				String tile = (char)y + String.valueOf(x);
 				boardMap.put(tile, false);
+			}
+		}
+		/* a b c d e f g h
+		 * 0 1 2 3 4 5 6 7
+		 */
+		int y = 'a';
+		for (int x = 1; x < 10; x += 2) {
+			if(x == 9) {
+				x = 0;
+				y++;
+			}else if(x == 8) {
+				x = 1;
+				y++;
+			}
+			if(y == 'd') {
+				y = 'f';
+			}
+			String tile = (char)y + String.valueOf(x);
+			boardMap.put(tile, false);
+			
+			pieceMap.put(tile, 000);
+			//king, alive, black
+			
+			if(tile.equals("h6")) {
+				x = 10;
 			}
 		}
 		
@@ -44,13 +69,34 @@ public class Checkers extends JFrame {
 	private class Clicklistener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
 			if (e.getSource() == forfeit) {
-				if(whiteTurn) {
-					blackWins++;
-				}else if(blackTurn) {
-					whiteWins++;
-				}
-				new Checkers();
+				onForfeit();
 			}
 		}
 	}
+	
+	public void onForfeit() {
+		if(whiteTurn) {
+			blackWins++;
+		}else if(blackTurn) {
+			whiteWins++;
+		}
+		new Checkers();
+	}
+	
+	public void onTile() {
+		
+	}
+	
+	public void onPiece() {
+		
+	}
+	
+	public void check() {
+		if(whiteTurn) {
+			
+		}else if(blackTurn) {
+			
+		}
+	}
+	
 }
